f0bd183cba5cb0730b3b1a1d51a9c1b6d0478393
tamotsu tanabe <tamotsu@sightlineapplications.com>
2020-02-24 19:35:07 -0800

    Changed indentation size to 2, added VS code format setting, added gitignore, changed local port to 5000.

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname));
 
-app.get("/", function(req,res) {
-    res.render("index");
+app.get("/", function (req, res) {
+  res.render("index");
 })
 
 app.listen(port, function () {
-    console.log("app running");
+  console.log("app running");
 })
